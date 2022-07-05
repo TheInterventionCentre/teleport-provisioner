d00a16d996bb53c1d0a40a1cd9ebf9768f610eb1
Rafael Palomar <rafael.palomar@ous-research.no>
2022-07-05 13:38:14 +0200

    ENH: Add provision of instance with teleport

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+resource "google_compute_address" "static" {
+  project   = "teleport-gateway-7717"
+  name      = "ipv4-address"
+  region    = "europe-north1"
+}
+
+
 # this creates a micro instance to host our teleport-gateway
 resource "google_compute_instance" "default" {
   name         = "teleport-gateway"
@@ -16,9 +23,8 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-
     access_config {
-      // Ephemeral public IP
+      nat_ip = google_compute_address.static.address
     }
   }
 
@@ -26,7 +32,23 @@ resource "google_compute_instance" "default" {
     foo = "bar"
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = <<SCRIPT
+    sudo apt update
+    sudo apt dist-upgrade -y
+    sudo apt install docker.io -y
+    sudo systemctl enable docker --now
+    useradd -m teleport
+    mkdir -p ~/teleport/config ~/teleport/data
+    docker run --hostname localhost --rm \
+               --entrypoint=/bin/sh \
+               -v ~/teleport/config:/etc/teleport \
+               quay.io/gravitational/teleport:9.3.7 -c "teleport configure > /etc/teleport/teleport.yaml"
+    docker run --hostname localhost --name teleport \
+               -v ~/teleport/config:/etc/teleport \
+               -v ~/teleport/data:/var/lib/teleport \
+               -p 3023:3023 -p 3025:3025 -p 3080:3080 \
+               quay.io/gravitational/teleport:9.3.7
+    SCRIPT
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
